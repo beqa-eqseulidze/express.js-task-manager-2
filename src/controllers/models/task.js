bd44e935d7d4cd8ae3c02101a1cd4c9a3409de51
beqa eqseulidze <beqaeqseulidze6@gmail/com>
2023-12-07 13:02:48 +0400

    add validation task model

--- src/controllers/models/task.js
+++ src/controllers/models/task.js
@@ -2,15 +2,16 @@ const mongoose=require('mongoose')
 const schema = new mongoose.Schema({
      title: {
         type:String,
+        unique:true,
         required:true,
         trim:true,       
         minLength:2,
         maxLength:200,
      }, 
      completed:{
-        type:Boolean,
-        unique: true,
-        required:true,        
+       type:Boolean,
+       required:true,   
+       default:false     
      }
      },{
       timestamps: true
